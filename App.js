01fc9373f648e3699e051ad2cd1397133af68651
5j <jnegrete15@ucol.mx>
2019-11-13 08:33:42 -0600

    almost ready

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import LoginView from './src/LoginView';
+import HomeView from './src/HomeView';
+import {Actions, Scene, Router} from 'react-native-router-flux';
+
+const scenes = Actions.create( 
+  <Scene key="root">
+    <Scene key="Login" component={LoginView} hideNavBar/>
+    <Scene key="home" component={HomeView} hideNavBar/>
+  </Scene>
+);
+
+type Props = {};
+export default class App extends Component<Props>{
+  render(){
+    return <Router scenes={scenes}/>
+  }
+}
---
+++ src/ArtistList.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {
+    ListView,
+    TouchableOpacity
+} from 'react-native';
+
+import ArtistBox from './ArtistBox'
+
+import {Actions} from 'react-native-router-flux';
+
+export default class ArtistList extends Component<Props> {
+
+    constructor(props) {
+        super();
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            DataSource: ds
+        }
+    }
+    componentDidMount() {
+        this.updateDataSource(this.props.artists)
+    }
+    componentWillReceiveProps(newProps){
+        if( newProps.artists !== this.props.artists) {
+            this.updateDataSource(newProps.artists)
+        }
+    }
+    updateDataSource = (data) => {
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(data)
+        })
+    }
+    handlePress(artist){
+        Actions.artistDetail({artist: artist})
+    }
+    render(){
+        return (
+            <ListView
+            enableEmptySections = {true}
+                dataSource={this.state.dataSource}
+                renderRow={(artist) => {
+                    return (
+                        <TouchableOpacity onPress={() => this.handlePress(artist)}>
+                            <ArtistBox artist={artist}/>
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+        );
+    }
+}
